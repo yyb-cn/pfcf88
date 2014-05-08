90728ec7a171540b4d1a1dea9964437213e651c2
yu yongbin <888@pfcf.cn>
2014-05-08 17:17:20 +0800

    支付查询接口

---
+++ 95epay_adminquery.php
@@ -0,0 +1,101 @@
+
+<?php
+// +----------------------------------------------------------------------
+// | Fanwe 方维p2p借贷系统
+// +----------------------------------------------------------------------
+// | Copyright (c) 2011 http://www.fanwe.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: 云淡风轻(88522820@qq.com)
+// +----------------------------------------------------------------------
+
+
+//用于处理 api同步登录的回调
+
+/*require './system/common.php';
+require './app/Lib/app_init.php';
+$payment_notice_id = intval($_REQUEST['id']);
+if(!$payment_notice_id){
+	$data['status'] = 0;
+	echo json_encode($data);
+}
+if(file_exists(APP_ROOT_PATH.'system/payment/Sqepay_payment.php'))
+{
+	require_once APP_ROOT_PATH.'system/payment/Sqepay_payment.php';
+	
+	$p_obj = new Sqepay_payment();
+	$p_obj->adminquery($payment_notice_id);
+}*/
+
+?>
+<?php
+header("Content-Type: text/html; charset=UTF-8");
+require './system/common.php';
+require './app/Lib/app_init.php';
+$payment_notice_id = intval($_REQUEST['id']);
+
+    	$payment_notice = $GLOBALS['db']->getRow("select * from ".DB_PREFIX."payment_notice where id = ".$payment_notice_id);
+		$order = $GLOBALS['db']->getRow("select order_sn,bank_id from ".DB_PREFIX."deal_order where id = ".$payment_notice['order_id']);
+		
+		$_TransID = $order['order_sn'];
+		
+		$payment_info = $GLOBALS['db']->getRow("select config from ".DB_PREFIX."payment where id=".intval($payment_notice['payment_id']));
+		$payment_info['config'] = unserialize($payment_info['config']);
+       
+		//$_MerchantID = $payment_info['config']['baofoo_account'];
+        //$_Md5Key = $payment_info['config']['baofoo_key'];
+		
+	$MerNo 				= "181138";
+	$MD5key      		= "aWOv]Fct";
+	$BillNo 			= $_TransID;
+	$MerUrl 			= "http://www.pfcf88.com/95epay_callback.php?act=query";
+	$MD5Info 			= getSignature($MerNo, $BillNo, $MerUrl, $MD5key);
+	print $MD5Info;
+	
+$post_data = array();  
+$post_data['MerNo']  = $MerNo;  
+$post_data['BillNo'] = $BillNo;  
+$post_data['MerUrl'] = $MerUrl; 
+$post_data['MD5Info'] = $MD5Info; 
+
+function getSignature($MerNo, $BillNo, $MerUrl, $MD5key){
+	$_SESSION['MerNo'] = $MerNo;
+	$_SESSION['MD5key'] = $MD5key;
+	$sign_params  = array(
+        'BillNo'       => $BillNo, 
+        'MerNo'       => $MerNo,
+        'MerUrl'       => $MerUrl
+    );
+  $sign_str = "";
+  ksort($sign_params);
+  foreach ($sign_params as $key => $val) {
+                               
+       $sign_str .= sprintf("%s=%s&", $key, $val);                
+                
+   }
+   print $sign_str;print '<br/><br/><br/>';
+   return strtoupper(md5($sign_str.strtoupper(md5($MD5key))));   
+
+	
+}
+
+
+function curl_post($url, $post) {  
+    $options = array(  
+        CURLOPT_RETURNTRANSFER => true,  
+        CURLOPT_HEADER         => false,  
+        CURLOPT_POST           => true,  
+        CURLOPT_POSTFIELDS     => $post,  
+    );  
+
+
+    $ch = curl_init($url);  
+    curl_setopt_array($ch, $options);  
+    $result = curl_exec($ch);  
+    curl_close($ch);  
+    return $result;  
+}  
+  
+$data = curl_post("http://www.95epay.cn/ReconciliationPort", $post_data);  
+  
+var_dump($data);  
+?>
