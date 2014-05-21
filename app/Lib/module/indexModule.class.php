e07e5a8d3f85fc8f81943e4afb7570e9c1d4cbdb
Zhai Jia <19965904@qq.com>
2014-05-21 17:55:38 +0800

    产品直接订购模块

--- app/Lib/module/indexModule.class.php
+++ app/Lib/module/indexModule.class.php
@@ -80,6 +80,14 @@ class indexModule extends SiteBaseModule
 			
 			$GLOBALS['tmpl']->assign("show_site_titile",1);
 		}
+		//lu 马上购买 
+		// 广西城建三期
+		$chengjian= get_deal_list(1,0,"publish_wait =0 AND deal_status=1 AND cate_id=5  "," id DESC");
+		// 信贷赢3号
+		$credit= get_deal_list(1,0,"publish_wait =0 AND deal_status=1 AND cate_id=3  "," id DESC");
+			$GLOBALS['tmpl']->assign("chengjian",$chengjian['list'][0]);
+			$GLOBALS['tmpl']->assign("credit",$credit['list'][0]);
+
 		
 		$GLOBALS['tmpl']->display("page/index.html",$cache_id);
 	}
