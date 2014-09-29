ab9ac4a8863fa7363942b756b22be0fdba182734
Colorfulin <313616432@qq.com>
2014-09-29 14:41:22 +0800

    优化手机端的判断

--- app/Lib/module/dayModule.class.php
+++ app/Lib/module/dayModule.class.php
@@ -13,6 +13,14 @@ class dayModule extends SiteBaseModule
 	public function index()
 	{	
 	
+	
+	
+		require_once 'Mobile_Detect.php';
+		$detect = new Mobile_Detect;
+		$deviceType = ($detect->isMobile() ? ($detect->isTablet() ? 'tablet' : 'phone') : 'computer');
+		
+		
+		
 		$data['time']=time();
 	
 		$data['yn']=rand(10000000,99999999);
@@ -23,13 +31,14 @@ class dayModule extends SiteBaseModule
 		
 		$GLOBALS['db']->autoExecute(DB_PREFIX."day",$data,"INSERT","","SILENT");
 	
-		if($this->check_wap())
+		if($deviceType=='computer')
 		{
-				$GLOBALS['tmpl']->display("page/day/mobile/day_mobile_index.html");//手机端
-		
+				
+			$GLOBALS['tmpl']->display("page/day/day_index.html");//浏览器端
 		}
 		else{
-		$GLOBALS['tmpl']->display("page/day/day_index.html");//浏览器端
+		$GLOBALS['tmpl']->display("page/day/mobile/day_mobile_index.html");//手机端
+	
 			}
 	
 		
@@ -59,56 +68,7 @@ class dayModule extends SiteBaseModule
 	}
 	
 	
-	function check_wap()
-{
-if (strpos(strtoupper($_SERVER['HTTP_ACCEPT']),"VND.WAP.WML") > 0)
-{
-// Check whether the browser/gateway says it accepts WML.
-	$br = "WML";
-}
-else
-{
-		$browser=substr(trim($_SERVER['HTTP_USER_AGENT']),0,4);
-		if ($browser=="Noki" || // Nokia phones and emulators
-		$browser=="Eric" || // Ericsson WAP phones and emulators
-		$browser=="WapI" || // Ericsson WapIDE 2.0
-		$browser=="MC21" || // Ericsson MC218
-		$browser=="AUR" || // Ericsson R320
-		$browser=="R380" || // Ericsson R380
-		$browser=="UP.B" || // UP.Browser
-		$browser=="WinW" || // WinWAP browser
-		$browser=="UPG1" || // UP.SDK 4.0
-		$browser=="upsi" || // another kind of UP.Browser ??
-		$browser=="QWAP" || // unknown QWAPPER browser
-		$browser=="Jigs" || // unknown JigSaw browser
-		$browser=="Java" || // unknown Java based browser
-		$browser=="Alca" || // unknown Alcatel-BE3 browser (UP based?)
-		$browser=="MITS" || // unknown Mitsubishi browser
-		$browser=="MOT-" || // unknown browser (UP based?)
-		$browser=="My S" ||// unknown Ericsson devkit browser ?
-		$browser=="WAPJ" || // Virtual WAPJAG www.wapjag.de
-		$browser=="fetc" || // fetchpage.cgi Perl script from www.wapcab.de
-		$browser=="ALAV" || // yet another unknown UP based browser ?
-		$browser=="Wapa" || // another unknown browser (Web based "Wapalyzer"?)
-					$browser=="Oper") // Opera
-			{
-			$br = "WML";
-			}
-			else
-			{
-			$br = "HTML";
-			}
-			}
-			if($br == "WML")
-			{
-			return TRUE;//手机端
-			}
-			else
-			{
-			return FALSE;//客户端
-			}
-	} 
-
+	
 	
 	
 }
