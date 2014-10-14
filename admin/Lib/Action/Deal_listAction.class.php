25dae3aafdab93392dfa7e9cc041dc5c14f2df68
Colorfulin <313616432@qq.com>
2014-10-14 11:22:20 +0800

    交易列表排序，搜索功能

--- admin/Lib/Action/Deal_listAction.class.php
+++ admin/Lib/Action/Deal_listAction.class.php
@@ -3,16 +3,56 @@
 class Deal_listAction extends CommonAction{
 	public function index()
 	{
-		$condition='';
+		$condition=' d.deal_status in(1,2,4,5) ';
 		if(trim($_REQUEST['user_name'])!='')
 		{
 			//echo $_REQUEST['user_name'];
-			$condition .= "d1.user_name =".$_REQUEST['user_name'];
+			$condition .= " and  dl.user_name ="."'".$_REQUEST['user_name']."'";
 		}
-			$condition .= "   d.deal_status in(1,2,4,5)  ";
-	
+		
+		if(trim($_REQUEST['name'])!='')
+		{
+			//echo $_REQUEST['user_name'];
+			$condition .= "and   d.name ="."'".$_REQUEST['name']."'";
+		}
+		
+		//print_r($_REQUEST);exit;
+		if(trim($_REQUEST['_sort'])==0){
+			$sort='desc';
+		
+		}
+		elseif(trim($_REQUEST['_sort'])==1){
+			$sort='asc';
+		
+		}
+		if(trim($_REQUEST['_order'])=='name')
+		{
+			$order=	"order  by  d.name  ".$sort;
+		}
+		elseif(trim($_REQUEST['_order'])=='deal_load_id')
+		{
+			$order=	"order  by deal_load_id ".$sort;
+		}
+		elseif(trim($_REQUEST['_order'])=='user_name')
+		{
+			$order=	"order  by dl.user_name ".$sort;
+		}
+		elseif(trim($_REQUEST['_order'])=='deal_time')
+		{
+			$order=	"order  by deal_time ".$sort;
+		}
+		elseif(trim($_REQUEST['_order'])=='u_load_money')
+		{
+			$order=	"order  by u_load_money ".$sort;
+		}
+		elseif(trim($_REQUEST['_order'])=='repay_time')
+		{
+			$order=	"order  by d.repay_time ".$sort;
+		}
+		//echo $order;exit;
+	//print_r($_REQUEST);exit;
 			
-    	$sql = "select d.name,d.repay_time,d.repay_time_type,dl.user_name,dl.money as u_load_money,dl.id as deal_load_id,dl.create_time as deal_time from ".DB_PREFIX."deal d left join ".DB_PREFIX."deal_load as dl on d.id = dl.deal_id LEFT JOIN ".DB_PREFIX."user u ON u.id=d.user_id where  d.deal_status in(1) order by dl.create_time desc";
+    	$sql = "select d.name,d.repay_time,d.repay_time_type,dl.user_name,dl.money as u_load_money,dl.id as deal_load_id,dl.create_time as deal_time from ".DB_PREFIX."deal d left join ".DB_PREFIX."deal_load as dl on d.id = dl.deal_id LEFT JOIN ".DB_PREFIX."user u ON u.id=d.user_id where ".$condition." and d.deal_status in(1)  " .$order;
 		
 		$list = $GLOBALS['db']->getAll($sql);
 		
