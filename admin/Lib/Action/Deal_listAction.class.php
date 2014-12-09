c30edc0e4b0ec9a72525cb5f59262cfe8c05343f
Colorfulin <313616432@qq.com>
2014-12-09 13:29:33 +0800

    会员搜索,项目预览

--- admin/Lib/Action/Deal_listAction.class.php
+++ admin/Lib/Action/Deal_listAction.class.php
@@ -4,7 +4,8 @@ class Deal_listAction extends CommonAction{
 	public function index()
 	{
 		$condition=' d.deal_status in(1,2,4,5) ';
-		
+		$group_list = M("UserGroup")->findAll();
+		$this->assign("group_list",$group_list);
 		if(trim($_REQUEST['user_name'])!='')
 		{
 			
@@ -20,7 +21,10 @@ class Deal_listAction extends CommonAction{
 		{
 			$condition .= "and   dl.id ="."'".$_REQUEST['deal_load_id']."'";
 		}
-		
+		if(trim($_REQUEST['group_id']!=0))
+		{
+			$condition .= "and   u.group_id ="."'".$_REQUEST['group_id']."'";
+		}
 		if(trim($_REQUEST['_sort'])==0){
 			$sort='desc';
 		
@@ -67,11 +71,12 @@ class Deal_listAction extends CommonAction{
 		{
 			$order=	"order  by group_name ".$sort;
 		}
-    	$sql = "select g.name as group_name, d.name,d.repay_time,d.repay_time_type,dl.user_name,dl.user_id,dl.money as u_load_money,dl.id as deal_load_id,dl.create_time as deal_time , dl.deal_load_check_yn from ".DB_PREFIX."deal d left join ".DB_PREFIX."deal_load as dl on d.id = dl.deal_id LEFT JOIN ".DB_PREFIX."user u ON u.id=dl.user_id  left join ".DB_PREFIX."user_group as g on u.group_id = g.id  where ".$condition .' '. $order;
+    	$sql = "select g.name as group_name, d.name,d.repay_time,d.repay_time_type,d.id as deal_id,dl.user_name,dl.user_id,dl.money as u_load_money,dl.id as deal_load_id,dl.create_time as deal_time , dl.deal_load_check_yn from ".DB_PREFIX."deal d left join ".DB_PREFIX."deal_load as dl on d.id = dl.deal_id LEFT JOIN ".DB_PREFIX."user u ON u.id=dl.user_id  left join ".DB_PREFIX."user_group as g on u.group_id = g.id  where ".$condition .' '. $order;
 		/*
 		d   是  deal
 		dl  是  deal_load
 		u   是  user
+		g   是  user_group
 		*/
 		$list = $GLOBALS['db']->getAll($sql);
 		//deal_load_check_yn
