30f29cebc3850204016812cf65b7f1bd4bb884c8
Colorfulin <313616432@qq.com>
2014-12-19 16:32:05 +0800

    操作备注加个 '号

--- admin/Lib/Action/UserCarryAction.class.php
+++ admin/Lib/Action/UserCarryAction.class.php
@@ -137,7 +137,7 @@ class UserCarryAction extends CommonAction{
 		$v['user_name']=M("User")->where("id=".$v['user_id'])->getField("user_name");
 		$v['bank_name'] =  M("bank")->where("id=".$v['bank_id'])->getField("name");
 		$arr[0]=array('序号','银行','地区(省)','地区(市/区)','支行名','开户名','卡号','金额','电话号码','操作备注','备注');
-		$arr[$k+1]=array($k+1,$v['bank_name'],$v['region_lv2_name'],$v['region_lv3_name'],$v['bankzone'],$v['real_name'],"'".$v['bankcard'],$v['money'],$v['phone'],$v['desc'],$v['user_name']);
+		$arr[$k+1]=array($k+1,$v['bank_name'],$v['region_lv2_name'],$v['region_lv3_name'],$v['bankzone'],$v['real_name'],"'".$v['bankcard'],$v['money'],$v['phone'],"'".$v['desc'],$v['user_name']);
 		}
 		
 		
