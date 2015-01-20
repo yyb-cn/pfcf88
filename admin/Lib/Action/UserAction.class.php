15104fd6ebc5204e8d38e3edc9054b0530b32d15
Colorfulin <313616432@qq.com>
2015-01-20 15:44:33 +0800

    用户列表，明细： 账户余额显示疑似做好了

--- admin/Lib/Action/UserAction.class.php
+++ admin/Lib/Action/UserAction.class.php
@@ -577,10 +577,78 @@ class UserAction extends CommonAction{
 		
 		$model = M ("UserLog");
 		if (! empty ( $model )) {
-			$this->_list ( $model, $map );
+				//排序字段 默认为主键名
+				if (isset ( $_REQUEST ['_order'] )) {
+					$order = $_REQUEST ['_order'];
+				} else {
+					$order = ! empty ( $sortBy ) ? $sortBy : $model->getPk ();
+				}
+				//排序方式默认按照倒序排列
+				//接受 sost参数 0 表示倒序 非0都 表示正序
+				if (isset ( $_REQUEST ['_sort'] )) {
+					$sort = $_REQUEST ['_sort'] ? 'asc' : 'desc';
+				} else {
+					$sort = $asc ? 'asc' : 'desc';
+				}
+				//取得满足条件的记录数
+				$count = $model->where ( $map )->count ( 'id' );
+				
+				if ($count > 0) {
+					//创建分页对象
+					if (! empty ( $_REQUEST ['listRows'] )) {
+						$listRows = $_REQUEST ['listRows'];
+					} else {
+						$listRows = '';
+					}
+					$p = new Page ( $count, $listRows );
+					//分页查询数据
+
+					$voList = $model->where($map)->order( "`" . $order . "` " . $sort)->limit($p->firstRow . ',' . $p->listRows)->findAll ( );
+					$new_list= $model->where($map)->order("id asc")->findAll ( );
+					
+					foreach($voList as $k=>$v){
+					$money=0;
+						foreach($new_list as $kk=>$vv){
+						
+							$money+=$vv['money'];
+							
+							if($v['id']==$vv['id']){
+								break;
+							}
+						}
+					$voList[$k]['cur_money']=$money;
+					
+					}
+					
+		//			echo $model->getlastsql();
+					//分页跳转的时候保证查询条件
+					foreach ( $map as $key => $val ) {
+						if (! is_array ( $val )) {
+							$p->parameter .= "$key=" . urlencode ( $val ) . "&";
+						}
+					}
+					//分页显示
+
+					$page = $p->show ();
+					//列表排序显示
+					$sortImg = $sort; //排序图标
+					$sortAlt = $sort == 'desc' ? l("ASC_SORT") : l("DESC_SORT"); //排序提示
+					$sort = $sort == 'desc' ? 1 : 0; //排序方式
+					//模板赋值显示
+					$this->assign ( 'list', $voList );
+					$this->assign ( 'sort', $sort );
+					$this->assign ( 'order', $order );
+					$this->assign ( 'sortImg', $sortImg );
+					$this->assign ( 'sortType', $sortAlt );
+					$this->assign ( "page", $page );
+					$this->assign ( "nowPage",$p->nowPage);
+				}
+				
+					//$this->_list ( $model, $map );
+			
 		}
 		$this->display ();
-		return;
+		
 	}
 	
 	public function passed(){
